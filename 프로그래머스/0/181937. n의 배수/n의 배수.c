cbd5442cabb3f79fd248cdbfe2eb1e51236fc730
Yurim <121031400+yurim-kim22@users.noreply.github.com>
2024-07-29 21:18:59 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 4.43 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num, int n) {
+    int answer = 0;
+    
+    if(num % n == 0){
+        return answer = 1;
+    }else{
+        return answer;
+    }
+    
+    return answer;
+}
