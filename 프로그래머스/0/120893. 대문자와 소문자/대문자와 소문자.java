978f1d585d4a4d752a51e6965254c2d0622002af
Yurim <121031400+yurim-kim22@users.noreply.github.com>
2024-08-26 22:14:27 +0900

    [level 0] Title: 대문자와 소문자, Time: 15.70 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,20 +1,20 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder result = new StringBuilder();
+        String answer = "";
         
         for (int i = 0; i < my_string.length(); i++) {
             char c = my_string.charAt(i);
             
             // 대문자인 경우 소문자로 변환
             if (Character.isUpperCase(c)) {
-                result.append(Character.toLowerCase(c));
+                answer += Character.toLowerCase(c);
             } 
             // 소문자인 경우 대문자로 변환
             else if (Character.isLowerCase(c)) {
-                result.append(Character.toUpperCase(c));
+                answer += Character.toUpperCase(c);
             }
         }
         
-        return result.toString();
+        return answer;
     }
 }
