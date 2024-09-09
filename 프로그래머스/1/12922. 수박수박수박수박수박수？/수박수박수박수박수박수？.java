8aa12c564133f7e4c1529d10a83b149b32b0e914
Yurim <121031400+yurim-kim22@users.noreply.github.com>
2024-09-09 21:46:35 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 52.81 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,8 +2,8 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         
-        for(int i = 0; i < n; i++){
-            if(i % 2 == 0){
+        for(int i = 1; i <= n; i++){
+            if(i % 2 != 0){
                 answer += '수';
                 }else {
                 answer += '박';
